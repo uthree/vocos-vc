342e8af404886f3297d87f9608284760b99314af
uthree <uthree271828@gmail.com>
2023-10-21 22:16:26 +0900

    Improve fine-tuning

--- train.py
+++ train.py
@@ -142,7 +142,10 @@ for epoch in range(args.epoch):
             loss_feat = D.feat_loss(cut_center_wav(wave_recon), cut_center_wav(wave))
 
             loss_adv = 0
-            logits = D.logits(cut_center_wav(wave_fake))
+            if args.finetune:
+                logits = D.logits(cut_center_wav(wave_recon))
+            else:
+                logits = D.logits(cut_center_wav(wave_fake))
             for logit in logits:
                 loss_adv += (logit ** 2).mean()
             loss_g = loss_adv + loss_mel * args.mel + loss_kl * args.kl + args.content * loss_con + loss_feat * args.feature_matching
